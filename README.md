ba2f7d2857d1db318c8a5ccbb9db17e9095b0ca0
bruno berrehuel <berrehuelbruno@gmail.com>
2016-09-22 21:36:25 +0000

    add icons, datas and text

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ## To-Do
-1. add smartphone configuration
-2. add detailed view (modal mode ?) on click on future day
+2. add detailed view (modal mode ?) on click on future day ?
 2. cards arrive from left (or flip ?), with little delay, when forecast is known
 2. smooth transition for card mouvement
 2. chose between autodetect or add a cityname
@@ -10,6 +9,7 @@
 6. add name of day after tomorrow on a header for each card
 7. smartphone : search bar under button
 8. choose degres and auto/manual : hide checkboxes with same name behind, in order to have only one choice 
-9. click on future view : flip today view with detailed info
-10. Faire apparapitre les données détaillées en cliquant sur une flêche vers le bas.
+9. click on future view : flip today view with detailed info ?
+10. Faire apparaitre les données détaillées en cliquant sur une flêche vers le bas.
 11. Vue par défaut : icone et température actuelle + icone indiquant un click
+12. 
--- script.js
+++ script.js
@@ -9,34 +9,82 @@
         var conditionUrl="https://api.wunderground.com/api/" + apiKey + "/conditions/q/" + maLatitude + "," + maLongitude + ".json";
         // display current conditions
         $.getJSON(conditionUrl, function (json) {
-          var city=json.current_observation.display_location.city;
-          var actualTemp_c=json.current_observation.temp_c;
-          var actualFeelTemp_c=json.current_observation.feelslike_c;
-          var actualIcon=json.current_observation.icon_url;
-          var relativeHumidity=json.current_observation.relative_humidity;
-          var windDir=json.current_observation.wind_dir;
-          var wind=json.current_observation.wind_kph;
-          var windGust=json.current_observation.wind_gust_kph;
-          var pressureMb=json.current_observation.pressure_mb;
-          var uv=json.current_observation.UV;
-          var dewPoint_c=json.current_observation.dewpoint_c;
-          $("#city").html("City of " + city);
-          $("#actualIcon").attr("src", actualIcon)
-          $("#actualTemp").html("Temperature : " + actualTemp_c);
-          $("#actualFeelTemp").html("Feels like : " + actualFeelTemp_c);
-          $("#relativeHumidity").html("Humidity : " + relativeHumidity);
-          $("#dewPoint").html("Dew point : " + dewPoint_c);
-          $("#windDir").html("Wind direction : " + windDir);
-          $("#wind").html("Wind speed : " + wind + " kph");
-          $("#windGust").html("Wind gust : " + windGust + " kph");
-          $("#pressureMb").html("Atmospheric pressure : " + pressureMb + " mb");
-          $("#uv").html("UV indice : " + uv);
-          
-          // display forecast for today
-          
+            var city=json.current_observation.display_location.city;
+            $("#city").html("City of " + city);
+        // boucle if pour choisir ligne en farenheit ou celsius
+            var actualTemp_c=json.current_observation.temp_c;
+            $("#actualTemp").html("Temperature : " + actualTemp_c);
+            var actualFeelTemp_c=json.current_observation.feelslike_c;
+            $("#actualFeelTemp").html("Feels like : " + actualFeelTemp_c);
+            var dewPoint_c=json.current_observation.dewpoint_c;
+            $("#dewPoint").html("Dew point : " + dewPoint_c);
+        //change to have https and i icons
+            var actualIcon=json.current_observation.icon_url;
+            var iconUrl=actualIcon.slice(0,4) + "s" + actualIcon.slice(4);
+            var actualIconUrl=iconUrl.replace(/k/i,"i");
+            $("#actualIcon").attr("src", actualIconUrl);
+        //
+            var relativeHumidity=json.current_observation.relative_humidity;
+            $("#relativeHumidity").html("Humidity : " + relativeHumidity);
+            var windDir=json.current_observation.wind_dir;
+            $("#windDir").html("Wind direction : " + windDir);
+        // boucle if si farenheit -> basculer en mph
+            var wind=json.current_observation.wind_kph;
+            $("#wind").html("Wind speed : " + wind + " kph");
+            var windGust=json.current_observation.wind_gust_kph;
+            $("#windGust").html("Wind gust : " + windGust + " kph");
+        // boucle if si farenheit -> basculer en psi
+            var pressureMb=json.current_observation.pressure_mb;
+            $("#pressureMb").html("Atmospheric pressure : " + pressureMb + " mb");
+            var pressureTrend=json.current_observation.pressure_trend;
+            $("#pressureTrend").html("Pressure trend : " + pressureTrend + " mb");
+            var uv=json.current_observation.UV;
+            $("#uv").html("UV indice : " + uv);
+        // display forecast
+        var forecastUrl="https://api.wunderground.com/api/" + apiKey + "/forecast10day/q/" + maLatitude + "," + maLongitude + ".json";
+        $.getJSON(forecastUrl, function(json) {
+            var day=[];
+            var high=[];
+            var low=[];
+            var wind=[];
+            var windDir=[];
+            var humidity=[];
+            var iconUrl=[];
+            var precip=[];
+            var text=[];
+            for (var i=0; i<8; i++) {
+                day[i]=json.forecast.simpleforecast.forecastday[i].date.weekday;
+                $("#day" + [i]).html(day[i]);
+            // boucle if pour choisir ligne en farenheit ou celsius
+                high[i]=json.forecast.simpleforecast.forecastday[i].high.celsius;
+                $("#max"+[i]).html(high[i]);
+                low[i]=json.forecast.simpleforecast.forecastday[i].low.celsius;
+                $("#min"+[i]).html(low[i]);
+            // boucle if si farenheit -> basculer en mph
+                wind[i]=json.forecast.simpleforecast.forecastday[i].avewind.kph;
+                $("#wind"+[i]).html(wind[i]);
+                windDir[i]=json.forecast.simpleforecast.forecastday[i].avewind.dir;
+                $("#windDir"+[i]).html(windDir[i]);
+                humidity[i]=json.forecast.simpleforecast.forecastday[i].avehumidity;
+                $("#humidity"+[i]).html(humidity[i]);
+            // boucle if si farenheit -> basculer en in
+                precip[i]=json.forecast.simpleforecast.forecastday[i].qpf_allday.mm;
+                $("#precip"+[i]).html(precip[i]);
+            }
+            for (i=0; i<16; i++) {
+            //change to have https url and i icons
+                iconUrl[i]=json.forecast.txt_forecast.forecastday[i].icon_url;
+                iconUrl[i]=iconUrl[i].slice(0,4) + "s" + iconUrl[i].slice(4);
+                iconUrl[i]=iconUrl[i].replace(/k/i, "i");
+                $("#picDay" + [i]).attr("src",iconUrl[i]);
+            //
+            //boucle if si farenheit -> basculer sur fctext
+            text[i]=json.forecast.txt_forecast.forecastday[i].fcttext_metric;
+            $("#text"+[i]).html(text[i]);
+            }
+          })
           // display hourly evolution for today
-          
-          // display forecast for future
+        var hourlyUrl="https://api.wunderground.com/api/" + apiKey + "/hourly/q/" + maLatitude + "," + maLongitude + ".json";
     }); 
     }
 
